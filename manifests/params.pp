259f999ed5efb8dc7ca79809b1ff11924302e7f4
tobkern <tobkern1980@googlemail.com>
2017-01-03 13:28:09 +0100

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class teamspeak::params {
     $init_file    = "teamspeak/init/${::osfamily}.init.erb"
     $systemd_file = 'teamspeak/systemd/teamspeak.erb'
-    $version      = '3.0.11.3'
+    $ts3version   = '3.0.11.3'
     $arch         = $::architecture
     $mirror       = 'http://dl.4players.de/ts/releases/<%=version%>/teamspeak3-server_linux-<%=download_arch%>-<%=version%>.tar.gz'
   
